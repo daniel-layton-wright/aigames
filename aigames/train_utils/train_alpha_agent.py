61739d313e42f9b5a38f1beca416403471f3e138
daniel-layton-wright <daniel.layton.wright@gmail.com>
2019-05-16 18:41:14 -0400

    Remove unused parameter from function signature

--- aigames/train_utils/train_alpha_agent.py
+++ aigames/train_utils/train_alpha_agent.py
@@ -1,7 +1,7 @@
 from aigames.agents import *
 
 
-def train_alpha_agent(game_class, model: AlphaModel, lr=0.01, monitor=None,
+def train_alpha_agent(game_class, model: AlphaModel, monitor=None,
                       n_games=10000, alpha_agent_kwargs=dict()):
     alpha_agent = AlphaAgent(game_class, model, **alpha_agent_kwargs)
     monitor = monitor(model, alpha_agent)
