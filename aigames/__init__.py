84b7735a7a5d394e8d8f3a5656ab089b5257ad45
daniel-layton-wright <daniel.layton.wright@gmail.com>
2020-05-02 16:43:20 -0500

    rebase

--- aigames/__init__.py
+++ aigames/__init__.py
@@ -1,10 +1,3 @@
-from .alpha_networks import *
-from .base import *
-from .games import *
-from .agents import *
-from .monitors import *
-from .play_against_minimax import *
-from .train_utils import *
 from .agent import *
 from .game import *
 from .training_manager import *
@@ -14,4 +7,3 @@ import torch.nn as nn
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
-
