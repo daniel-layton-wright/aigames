c2ce7c988dde259d947186db2a41b483777b654c
daniel-layton-wright <daniel.layton.wright@gmail.com>
2023-06-15 14:08:54 -0600

    Fix optuna study utility script

--- experiments/alpha/utils/copy_optuna_study_without_pruned.py
+++ experiments/alpha/utils/copy_optuna_study_without_pruned.py
@@ -6,19 +6,19 @@ def main():
     # Read in command line arguments for the source and destination study names
     parser = argparse.ArgumentParser()
     parser.add_argument('--source_study', '-s', type=str, required=True)
-    parser.add_argument('--source_study_name', type=str, required=True, default='tictactoe_lightning')
+    parser.add_argument('--source_study_name', type=str, required=False, default='tictactoe_lightning')
     parser.add_argument('--dest_study', '-d', type=str, required=True)
-    parser.add_argument('--dest_study_name', type=str, required=True, default='tictactoe_lightning')
+    parser.add_argument('--dest_study_name', type=str, required=False, default='tictactoe_lightning')
     args, _ = parser.parse_known_args()
 
     # Load the source study
     import optuna
     source_storage = JournalStorage(JournalFileStorage(args.source_study))
-    source_study = optuna.load_study(study_name=args.source_study_name, storage=source_storage, load_if_exists=True)
+    source_study = optuna.load_study(study_name=args.source_study_name, storage=source_storage)
 
     # Create the destination study
     dest_storage = JournalStorage(JournalFileStorage(args.dest_study))
-    dest_study = optuna.create_study(study_name=args.dest_study_name, storage=dest_storage, load_if_exists=True)
+    dest_study = optuna.create_study(study_name=args.dest_study_name, storage=dest_storage)
 
     # Copy the trials from the source to the destination if they are completed
     n_copied = 0
