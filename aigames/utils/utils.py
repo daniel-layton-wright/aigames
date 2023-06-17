f1c94fff9b39aa799433da591fe70d5961061643
daniel-layton-wright <daniel.layton.wright@gmail.com>
2023-06-17 15:12:00 -0600

    get_all_slots util function for hyperparam classes

--- aigames/utils/utils.py
+++ aigames/utils/utils.py
@@ -11,3 +11,12 @@ def play_tournament(game_class, players, n_games, discount_rate=0.99):
         rewards.append( reward_listener.reward )
 
     return float(sum(rewards)) / len(rewards)
+
+
+def get_all_slots(obj):
+    """
+    For an object with a __slots__ variable which may inherit from other classes with __slots__ variables, this
+    function collects all the slots into a single iterable.
+    """
+    from itertools import chain
+    return chain.from_iterable(getattr(cls, '__slots__', []) for cls in type(obj).__mro__)
