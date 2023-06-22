9dfec145ba64e2ae103b157994768cc7eb98f6c7
daniel-layton-wright <daniel.layton.wright@gmail.com>
2023-06-21 19:30:12 -0600

    Different attempt at command line game screen clearing

--- aigames/game/command_line_game.py
+++ aigames/game/command_line_game.py
@@ -1,23 +1,38 @@
 from .game import GameListener
 import os
 import time
+# TODO : do something about this
 
 
 class CommandLineGame(GameListener):
     def __init__(self, pause_time=1, clear_screen=True):
         self.pause_time = pause_time
         self.clear_screen = clear_screen
+        self.last_printed_lines = 0
 
     def before_action(self, game, legal_actions):
         if self.clear_screen:
-            os.system('cls' if os.name == 'nt' else 'clear')
-        print(str(game))
+            self.clear_n_lines(self.last_printed_lines)
+
+        out = str(game)
+        self.last_printed_lines = out.count('\n') + 1
+        print(out)
 
     def after_action(self, game):
         if self.clear_screen:
-            os.system('cls' if os.name == 'nt' else 'clear')
-        print(str(game))
+            self.clear_n_lines(self.last_printed_lines)
+
+        out = str(game)
+        self.last_printed_lines = out.count('\n') + 1
+        print(out)
         time.sleep(self.pause_time)
 
     def on_game_end(self, game):
         print('Game over.')
+
+    @staticmethod
+    def clear_n_lines(n):
+        if n == 0:
+            return
+
+        print(f'\033[{n}A\033[J', end='')
