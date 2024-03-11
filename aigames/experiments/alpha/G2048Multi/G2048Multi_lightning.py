Fix loss computation (unstable sometimes, use torch cross_entropy to handle) Add logic to data buffer so that you can fill it with more than one batch to slow down network updates

aigames/experiments/alpha/G2048Multi/G2048Multi_lightning.py
@@ -216,16 +216,15 @@ def main():
         hyperparams.dirichlet_epsilon = 0.1
         hyperparams.scaleQ = True
         hyperparams.c_puct = 2  # Can be low/normal when scaleQ is True
-        hyperparams.lr = 0.001
+        hyperparams.lr = 0.0003
         hyperparams.weight_decay = 1e-5
         hyperparams.training_tau = TrainingTauDecreaseOnPlateau([1.0, 0.7, 0.5, 0.3, 0.1, 0.0],
                                                                 'eval_game_avg_max_tile',
                                                                 4 * hyperparams.self_play_every_n_epochs)
         hyperparams.td_lambda = TDLambdaByRound([1.0, 0.5])  # [1, 0.9, 0.8, 0.7, 0.6, 0.5])
         hyperparams.batch_size = 1024
-        hyperparams.game_listeners = [ActionCounterProgressBar(1500, description='Train game action count'),
-                                      # game_progress_callback
-                                      ]
+        hyperparams.data_buffer_full_size = 16_384  # stabilize things by doing 16 steps before using new network for next TD estimates
+        hyperparams.game_listeners = [ActionCounterProgressBar(1500, description='Train game action count')]
         hyperparams.eval_game_listeners = [ActionCounterProgressBar(1500, description='Eval game action count')]
         hyperparams.discount = 0.999
         hyperparams.clear_dataset_before_self_play_rounds = []
@@ -275,6 +274,8 @@ def main():
     wandb.init(**wandb_kwargs)
     wandb_run = wandb.run.name or os.path.split(wandb.run.path)[-1]
     wandb.run.watch(training_run.network)
+
+    torch.autograd.set_detect_anomaly(True)
 
     model_checkpoint = ModelCheckpoint(dirpath=os.path.join(args.ckpt_dir, wandb_run), save_last=True)
     episode_history_checkpoint = EpisodeHistoryCheckpoint(os.path.join(args.ckpt_dir, wandb_run))

aigames/experiments/alpha/G2048Multi/network_architectures.py
@@ -101,36 +101,38 @@ class G2048MultiNetworkV2(AlphaMultiNetwork, BaseAlphaEvaluator):
 
         self.n_base_out_features = self.hyperparams.n_out_channels
 
-        self.policy_head = nn.Sequential(
+        self.policy_logits_head = nn.Sequential(
             nn.Linear(in_features=self.n_base_out_features, out_features=128),
             nn.ReLU(),
             nn.Linear(in_features=128, out_features=32),
             nn.ReLU(),
-            nn.Linear(in_features=32, out_features=4),
-            nn.Softmax(dim=1)
+            nn.Linear(in_features=32, out_features=4)
         )
 
-        self.value_head = nn.Sequential(
+        self.value_logits_head = nn.Sequential(
             nn.Linear(in_features=self.n_base_out_features, out_features=128),
             nn.ReLU(),
             nn.Linear(in_features=128, out_features=128),
             nn.ReLU(),
-            nn.Linear(in_features=128, out_features=self.hyperparams.n_value_buckets),
-            nn.Softmax(dim=1)
+            nn.Linear(in_features=128, out_features=self.hyperparams.n_value_buckets)
         )
 
     def forward(self, processed_state):
         base = self.base(processed_state)
-        policy = self.policy_head(base).squeeze()
-        value_bucket_softmax = self.value_head(base)
-        return policy, value_bucket_softmax
+        policy_logits = self.policy_logits_head(base)
+        value_bucket_logits = self.value_logits_head(base)
+        return policy_logits, value_bucket_logits
 
     @torch.no_grad()
     def evaluate(self, state):
         if state.shape[1] != 16:
             state = self.process_state(state)
 
-        pi, value_bucket_softmax = self.forward(state)
+        pi_logits, value_bucket_logits = self.forward(state)
+
+        pi = torch.softmax(pi_logits, dim=1)
+        value_bucket_softmax = torch.softmax(value_bucket_logits, dim=1)
+
         scaled_value = self.digitize(value_bucket_softmax)
         value = self.inverse_scale(scaled_value).unsqueeze(1)
         return pi, value
@@ -174,14 +176,15 @@ class G2048MultiNetworkV2(AlphaMultiNetwork, BaseAlphaEvaluator):
                 - sgn*torch.sqrt(sgn*hp/e + 1/(4*e*e) + 1)/e)
 
     def loss(self, states, pis, values, *args, **kwargs) -> Tuple[torch.Tensor, torch.Tensor]:
-        pred_distns, pred_values = self.forward(states)
+        pred_distn_logits, pred_value_logits = self.forward(states)
 
         nan_distns = torch.isnan(pis).any(dim=1)
 
         values = self.scale_and_bucketize(values)
 
-        value_loss = (-torch.sum(values * torch.log(pred_values), dim=1)).mean()
-        distn_loss = (-torch.sum(pis[~nan_distns] * torch.log(pred_distns[~nan_distns]), dim=1)).mean()
+        value_loss = nn.functional.cross_entropy(pred_value_logits, values)
+        distn_loss = nn.functional.cross_entropy(pred_distn_logits[~nan_distns], pis[~nan_distns])
+
         return value_loss, distn_loss
 
     @classmethod
@@ -198,6 +201,7 @@ class G2048MultiNetworkV2(AlphaMultiNetwork, BaseAlphaEvaluator):
 
 
 class G2048MultiNetworkV3(G2048MultiNetworkV2):
+    # TODO : fix this to use logit method for more stable loss same as V2
     def __init__(self, hyperparams: G2048MultiNetworkV2.Hyperparameters = G2048MultiNetworkV2.Hyperparameters()):
         super().__init__(hyperparams)
         self.num_moves_head = nn.Sequential(
@@ -210,8 +214,8 @@ class G2048MultiNetworkV3(G2048MultiNetworkV2):
 
     def forward(self, processed_state):
         base = self.base(processed_state)
-        policy = self.policy_head(base).squeeze()
-        value_bucket_softmax = self.value_head(base)
+        policy = self.policy_logits_head(base).squeeze()
+        value_bucket_softmax = self.value_logits_head(base)
         num_moves = self.num_moves_head(base)
         return policy, value_bucket_softmax, num_moves
 

aigames/training_manager/alpha_training_manager_multi.py
@@ -113,20 +113,25 @@ class TrajectoryDataset(AlphaDatasetMulti):
     num_items = 3
 
     class DataBuffer:
-        def __init__(self, num_items, batch_size):
+        def __init__(self, num_items, batch_size, full_size=None):
             self.data = [None for _ in range(num_items)]
             self.batch_size = batch_size
+            self.full_size = max(full_size, batch_size) if full_size is not None else batch_size
 
         def add_to_buffer(self, *args):
             for i, arg in enumerate(args):
                 self.data[i] = arg if self.data[i] is None else torch.cat((self.data[i], arg))
 
         def full(self):
-            return self.data[0].shape[0] >= self.batch_size
+            return self.data[0].shape[0] >= self.full_size
 
         def yield_data(self):
-            out = tuple(self.data[i][:self.batch_size] for i in range(len(self.data)))
-            self.data = [self.data[i][self.batch_size:] for i in range(len(self.data))]
+            # Yield a random subset of the data of size self.batch_size
+            batch_size = min(self.batch_size, self.data[0].shape[0])
+            random_order = np.random.permutation(self.data[0].shape[0])
+
+            out = tuple(self.data[i][random_order[:batch_size]] for i in range(len(self.data)))
+            self.data = [self.data[i][random_order[batch_size:]] for i in range(len(self.data))]
 
             return out
 
@@ -154,7 +159,8 @@ class TrajectoryDataset(AlphaDatasetMulti):
 
     def __iter__(self):
         random_order_of_trajectories = np.random.permutation(self.trajectories)
-        data_buffer = self.DataBuffer(num_items=self.num_items, batch_size=self.hyperparams.batch_size)
+        data_buffer = self.DataBuffer(num_items=self.num_items, batch_size=self.hyperparams.batch_size,
+                                      full_size=self.hyperparams.data_buffer_full_size)
 
         for cur_traj in random_order_of_trajectories:
             data_buffer.add_to_buffer(*self.get_data(cur_traj))
@@ -162,8 +168,8 @@ class TrajectoryDataset(AlphaDatasetMulti):
             while data_buffer.full():
                 yield data_buffer.yield_data()
 
-        if len(data_buffer) > 0:
-            data_buffer.yield_data()
+        while len(data_buffer) > 0:
+            yield data_buffer.yield_data()
 
     def get_data(self, cur_traj):
         self.evaluator.eval()